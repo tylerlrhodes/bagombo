2412142f7dea074ccd065ec1a77150b40472eb53
Tyler Rhodes <tylerlrhodes@gmail.com>
2017-11-19 02:16:45 -0500

    update to bootstrap 4 in progress, get rid of bower and move to npm

--- src/Bagombo/Gruntfile.js
+++ src/Bagombo/Gruntfile.js
@@ -12,11 +12,18 @@ module.exports = function (grunt) {
     copy: {
       build: {
         files: [
-          { expand: true, src: ['bower_components/**/*.min.css'], dest: 'wwwroot/lib/css/', filter: 'isFile', flatten: true },
-          { expand: true, src: ['bower_components/highlightjs/styles/default.css'], dest: 'wwwroot/lib/css/', rename: function (dest, src) { return dest + "highlight-default.css"; } },
+          //{ expand: true, src: ['bower_components/**/*.min.css'], dest: 'wwwroot/lib/css/', filter: 'isFile', flatten: true },
+          //{ expand: true, src: ['bower_components/highlightjs/styles/default.css'], dest: 'wwwroot/lib/css/', rename: function (dest, src) { return dest + "highlight-default.css"; } },
+          //{ expand: true, src: ['bower_components/**/*.min.js'], dest: 'wwwroot/lib/js/', filter: 'isFile', flatten: true },
+          //{ expand: true, src: ['bower_components/bootstrap/fonts/*'], dest: 'wwwroot/lib/fonts/', filter: 'isFile', flatten: true },
+          { expand: true, src: ['node_modules/highlightjs/styles/default.css'], dest: 'wwwroot/lib/css/', rename: function (dest, src) { return dest + "highlight-default.css"; } },
+          { expand: true, src: ['node_modules/bootstrap/**/*.min.css'], dest: 'wwwroot/lib/css/', filter: 'isFile', flatten: true },
+          { expand: true, src: ['node_modules/fontawesome/**/*.min.css'], dest: 'wwwroot/lib/css/', filter: 'isFile', flatten: true },
+          { expand: true, src: ['node_modules/highlightjs/**/*.min.js'], dest: 'wwwroot/lib/js/', filter: 'isFile', flatten: true },
+          { expand: true, src: ['node_modules/bootstrap/**/*.min.js'], dest: 'wwwroot/lib/js/', filter: 'isFile', flatten: true },
+          { expand: true, src: ['node_modules/popper.js/**/*.min.js'], dest: 'wwwroot/lib/js/', filter: 'isFile', flatten: true },
+          { expand: true, src: ['node_modules/jquery/dist/jquery.min.js'], dest: 'wwwroot/lib/js/', filter: 'isFile', flatten: true },
           { expand: true, src: ['css/*'], dest: 'wwwroot/lib/css/', filter: 'isFile', flatten: true },
-          { expand: true, src: ['bower_components/**/*.min.js'], dest: 'wwwroot/lib/js/', filter: 'isFile', flatten: true },
-          { expand: true, src: ['bower_components/bootstrap/fonts/*'], dest: 'wwwroot/lib/fonts/', filter: 'isFile', flatten: true },
           { expand: true, src: ['javascript/*.js'], dest: 'wwwroot/lib/js/', filter: 'isFile', flatten: true }
         ]
       }
